2fb7550edc15d72419b2a039b3a9e319c239b297
Angela Graham <angelagraham101@gmail.com>
2022-01-13 18:36:42 -0800

    put comments in js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 console.log ("hello");
 console.log ("love avacados!")
 console.log ("Angela Graham")
+// comments things out
+    
+    /* comments */
+
+    //TODO: Delete this after implementing feature 22
+   // let userPhoneNumber ="555-555-5555";
+
+    /* Every Good Boy Does Fine...
+        Please Excuse My Dear Aunt Sally...
+        On Old Olympus Mouintian Tops a Fin and German Viewed Some Hops...
+        Hey, you've got a lot of nerve!
+        Have you seen my supersuit? */
+
+    // OMG! The World became a Sci-fi Movie!
